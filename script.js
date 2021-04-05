bda5f459d1ab22febfa85dcb5b3f129f7ddfab5c
akp111 <pradhanashish95@gmail.com>
2021-04-05 16:20:37 +0530

    made changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function validate() {
   result += validateEmail();
   result += validatePassword();
   result += validateTerms();
-
+  result += validateDOB()
   if (result == "") return true;
 
   alert("Validation Result:\n\n" + result);
@@ -14,7 +14,7 @@ function validate() {
 function validateName() {
   var name = document.getElementsByName("name")[0].value;
   if (name.length <= 3)
-    return "Name should be at least three characters.\n";
+    document.getElementById("name_error").innerHTML="Name should be at least three characters.\n";
   return "";
 }
 
@@ -23,10 +23,10 @@ function validatePassword() {
   var retype = valueOf("retype_password");
 
   if (password.length <= 6)
-    return "Password should be at least 6 characters.\n";
+  document.getElementById("password_error").innerHTML="Password should be at least 6 characters.\n";
 
   if (password != retype)
-    return "Passwords do not match.\n";
+  document.getElementById("password_error").innerHTML="Passwords do not match.\n";
   return "";
 }
 
@@ -35,18 +35,29 @@ function validateEmail() {
   var retype = valueOf("retype_email");
 
   if (email.indexOf('@') == -1)
-    return "Email should be a valid address.\n";
+  document.getElementById("email_error").innerHTML="Email should be a valid address.\n";
 
   if (email != retype)
-    return "Email addresses do not match.\n";
+  document.getElementById("email_error").innerHTML="Email addresses do not match.\n";
   return "";
 }
 
-function validateTerms() {
-  var terms = document.getElementsByName("terms")[0];
-  if (!terms.checked)
-    return "Please accept the Terms of Service and Privacy Policy";
-  return "";
+// function validateTerms() {
+//   var terms = document.getElementsByName("terms")[0];
+//   if (!terms.checked)
+//   document.getElementById("password_error").innerHTML="Please accept the Terms of Service and Privacy Policy";
+//   return "";
+// }
+
+function validateDOB() {
+  var date = document.getElementsByName("date")[0].value;
+  if (!date)
+    return "Please eneter DOB"
+  var age = Math.floor((new Date() - new Date(date).getTime()) / 3.15576e+10)
+  if (age > 18)
+    return ""
+  else
+    return "Not old enough"
 }
 
 function valueOf(name) {
